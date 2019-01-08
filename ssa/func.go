b241e05eddb2daf5afeee12c4fd78319e5e6cbcf
Denis Isaev <denis@golangci.com>
2019-01-08 15:13:23 +0300

    improve

--- ssa/func.go
+++ ssa/func.go
@@ -1,14 +1,21 @@
 package ssa
 
-// numberRegisters assigns numbers to all SSA registers
-// (value-defining Instructions) in f, to aid debugging.
-// (Non-Instruction Values are named at construction.)
-//
-func NumberRegisters(instr interface{}, v int) {
+func Panics(instr interface{}, v int) {
     switch instr.(type) {
     case Value:
       instr.(interface {
         setNum(int)
       }).setNum(v)
+    }
+}
+
+type setNumable interface {
+  setNum(int)
+}
+
+func DoesNotPanic(instr interface{}, v int) {
+    switch instr.(type) {
+    case Value:
+      instr.(setNumable).setNum(v)
     }
 }
